b66aa2e67f999f2b23eb92e77613f6669c8466a4
arun <arunkumar12712@gmail.com>
2022-12-23 16:02:10 +0530

    State Logic Implements

---
+++ Quotation.Application/Repositories/Interface/IStateRepository.cs
@@ -0,0 +1,18 @@
+﻿using Quotation.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quotation.Application.Repositories.Interface
+{
+    public interface IStateRepository
+    {
+        List<State> GetAllUnits();
+        State CreateUnit(State state);
+        State UpdateUnit(State state);
+        State GetUnitById(int id);
+        void DeleteUnitById(State state);
+    }
+}
---
+++ Quotation.Infrastructure/Repository/StateRepository.cs
@@ -0,0 +1,56 @@
+﻿using Quotation.Application.Repositories.Interface;
+using Quotation.Domain.Entities;
+using Quotation.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quotation.Infrastructure.Repository
+{
+    public class StateRepository : IStateRepository
+    {
+        private readonly QuotationDbContext _quotationDbContext;
+
+        public StateRepository(QuotationDbContext quotationDbContext) 
+        {
+            _quotationDbContext = quotationDbContext;
+        }
+
+        public State CreateUnit(State state)
+        {
+            _quotationDbContext.Add(state);
+            _quotationDbContext.SaveChanges();
+            return state;
+        }
+
+        public void DeleteUnitById(State state)
+        {
+            _quotationDbContext.Remove(state);
+            _quotationDbContext.SaveChanges();  
+        }
+
+        public List<State> GetAllUnits()
+        {
+           return _quotationDbContext.States.ToList();
+        }
+
+        public State GetUnitById(int id)
+        {
+            var results = _quotationDbContext.States.Where(t => t.Id == id).SingleOrDefault();
+            if(results != null)
+            {
+                return results;
+            }
+            return new State();
+        }
+
+        public State UpdateUnit(State state)
+        {
+            var result= _quotationDbContext.Update(state);
+            _quotationDbContext.SaveChanges();
+            return result.Entity;
+        }
+    }
+}
