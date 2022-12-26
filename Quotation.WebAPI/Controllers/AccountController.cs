953bdbea96087b98a15552c74261477d4cf24445
arun <arunkumar12712@gmail.com>
2022-12-26 18:34:54 +0530

    identity working on

--- Quotation.WebAPI/Controllers/AccountController.cs
+++ Quotation.WebAPI/Controllers/AccountController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Quotation.Application.ViewModel;
 using Quotation.Infrastructure.Identity;
+using System.Net;
 using System.Security.Claims;
 
 namespace Quotation.WebAPI.Controllers
@@ -16,15 +17,19 @@ namespace Quotation.WebAPI.Controllers
     {
         private readonly SignInManager<ApplicationUser> _signInManager;
         private readonly UserManager<ApplicationUser> _userManager;
+        private RoleManager<IdentityRole> _roleManager;
         private readonly IMapper _mapper;
+
         public AccountController(
             SignInManager<ApplicationUser> signInManager,
             UserManager<ApplicationUser> userManager,
+            RoleManager<IdentityRole> roleManager,
             IMapper mapper
             )
         {
             _signInManager = signInManager;
             _userManager = userManager;
+            _roleManager = roleManager;
             _mapper = mapper;
         }
 
@@ -32,9 +37,9 @@ namespace Quotation.WebAPI.Controllers
         [Route("Register")]
         public async Task<IActionResult> Register(RegisterViewModel request)
         {
-            var user =_mapper.Map<ApplicationUser>(request);
+            var user = _mapper.Map<ApplicationUser>(request);
             var results = await _userManager.CreateAsync(user, request.Password);
-            if(results.Succeeded)
+            if (results.Succeeded)
             {
 
             }
@@ -75,5 +80,53 @@ namespace Quotation.WebAPI.Controllers
 
             return new BadRequestObjectResult(new { Message = "Logout successfully" });
         }
+
+        [HttpGet]
+        [Route("AllUsers")]
+        public IActionResult GetAllUsers()
+        {
+            return Ok(_mapper.Map<List<UserViewDetails>>(_userManager.Users.ToList()));
+        }
+
+        [HttpPost]
+        [Route("CreateRole")]
+        public async Task<IActionResult> CreateRole(RoleViewModel request)
+        {
+            IdentityResult result = new IdentityResult();
+            IdentityError? error = new IdentityError();
+            if (request != null)
+            {
+                 result = await _roleManager.CreateAsync(new IdentityRole() { Name = request.Name });
+                if (result.Succeeded)
+                {
+                    return Ok(result);
+                }
+                else
+                {
+                    error = result.Errors.FirstOrDefault();
+                    return new BadRequestObjectResult(new { Error = error });
+                }
+            }
+            return new BadRequestResult();
+        }
+        
+
+        [HttpGet]
+        [Route("AllRoles")]
+        public ActionResult<RoleViewModel> AllRoles()
+        {
+            return Ok(_mapper.Map<List<RoleViewModel>>(_roleManager.Roles));
+        }
+
+
+        [HttpPost]
+        [Route("AssignRoles")]
+        public async Task<IActionResult> AssignRoles(AssignRoleViewModel request)
+        {
+            var user = await _userManager.FindByNameAsync(request.Username);
+            var roles = await _userManager.AddToRoleAsync(user, request.Name);
+            return Ok(roles);
+        }
     }
 }
+
